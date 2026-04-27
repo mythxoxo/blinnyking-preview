f2286236e78d2f0d84ffced03f2cd320d300115b
krabster <krabster@krabster.krabster>
2026-04-28 00:02:30 +0300

    Fix Vercel middleware invocation for next-intl

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,12 @@
-export {default} from 'next-intl/middleware';
+import createMiddleware from 'next-intl/middleware';
+import {defaultLocale, locales} from './i18n/routing';
+
+export default createMiddleware({
+  locales,
+  defaultLocale,
+  localePrefix: 'always'
+});
+
 export const config = {
   matcher: ['/((?!api|trpc|_next|_vercel|.*\\..*).*)']
 };
